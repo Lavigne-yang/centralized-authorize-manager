da1e183bfe8e9e602b9d075a97b527fab2793b64
Lavigne-yang <2034549297@qq.com>
2023-08-29 22:29:28 +0800

    #CAM-0001 * 构建项目

---
+++ centralized-authorize-server/src/main/java/com/inge/sso/authorize/server/CamAuthorizeServerApplication.java
@@ -0,0 +1,15 @@
+package com.inge.sso.authorize.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lavyoung1325
+ */
+@SpringBootApplication
+public class CamAuthorizeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamAuthorizeServerApplication.class);
+    }
+}
